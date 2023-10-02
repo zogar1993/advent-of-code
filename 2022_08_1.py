b48b1be24ce507871b5ccf7e23b90b5956148e1f
Facundo Garcia <facundogarcia1993@gmail.com>
2023-10-02 02:51:37 -0300

    fix 2022_08_1

--- 2022_08_1.py
+++ 2022_08_1.py
@@ -9,35 +9,38 @@ row_length = len(lines[0])
 visible_trees_matrix = [[True for _ in range(row_length)] for _ in lines]
 
 for y in range(len(lines)):
-    highest = 0
+    highest = -1
     for x in range(row_length):
-        if lines[y][x] >= highest:
+        if lines[y][x] > highest:
             highest = lines[y][x]
             visible_trees_matrix[y][x] = False
+        if highest == 9:
+            break
 
-    highest = 0
+    highest = -1
     for x in range(row_length - 1, -1, -1):
-        if lines[y][x] >= highest:
+        if lines[y][x] > highest:
             highest = lines[y][x]
             visible_trees_matrix[y][x] = False
-
-for x in range(row_length):
-    highest = 0
-    for y in range(len(lines)):
-        if lines[y][x] >= highest:
-            highest = lines[y][x]
-            visible_trees_matrix[y][x] = False
-
-    highest = 0
-    for y in range(len(lines) - 1, -1, -1):
-        if lines[y][x] >= highest:
-            highest = lines[y][x]
-            visible_trees_matrix[y][x] = False
+        if highest == 9:
+            break
 
 result = 0
-for row in visible_trees_matrix:
-    for value in row:
-        if value:
+for x in range(row_length):
+    highest = -1
+    for y in range(len(lines)):
+        if lines[y][x] > highest:
+            highest = lines[y][x]
+            visible_trees_matrix[y][x] = False
+        if highest == 9:
+            break
+
+    highest = -1
+    for y in range(len(lines) - 1, -1, -1):
+        if lines[y][x] > highest:
+            highest = lines[y][x]
+            result += 1
+        elif not visible_trees_matrix[y][x]:
             result += 1
 
 print(result)
