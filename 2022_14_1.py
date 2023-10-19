97df93332b42d0b7e20d5b987d5d50cb5402d3e8
Facundo Garcia <facundogarcia1993@gmail.com>
2023-10-18 22:02:50 -0300

    complete 2022_14_1

--- 2022_14_1.py
+++ 2022_14_1.py
@@ -7,10 +7,9 @@ lines = text.split("\n")
 
 rock_lines = [[{"x": int(item[0]), "y": int(item[1])} for item in [item.split(",") for item in line.split(" -> ")]] for line in lines]
 
-
 min_x = sys.maxsize
 max_x = -sys.maxsize
-min_y = sys.maxsize
+min_y = 0
 max_y = -sys.maxsize
 
 for rock_line in rock_lines:
@@ -21,8 +20,6 @@ for rock_line in rock_lines:
             min_x = rock["x"]
         if rock["y"] > max_y:
             max_y = rock["y"]
-        if rock["y"] < min_y:
-            min_y = rock["y"]
 
 x_length = max_x - min_x + 1
 y_length = max_y - min_y + 1
@@ -31,7 +28,7 @@ for rock_line in rock_lines:
         rock["x"] -= min_x
         rock["y"] -= min_y
 
-cave = [[" " for _ in range(x_length)] for _ in range(y_length)]
+cave = [["." for _ in range(x_length)] for _ in range(y_length)]
 
 for rock_line in rock_lines:
     for i in range(len(rock_line) - 1):
@@ -44,6 +41,51 @@ for rock_line in rock_lines:
             for y in range(rock_a["y"], rock_b["y"] + y_direction, y_direction):
                 cave[y][x] = "#"
 
-for line in cave:
-    print(line)
+sand_starting_x = 500 - min_x
+cave[0][sand_starting_x] = "+"
 
+sand_path = [{"x": sand_starting_x, "y": 0}]
+sand_count = 0
+
+while True:
+    current_sand = sand_path[-1]
+    x = current_sand["x"]
+    y = current_sand["y"]
+
+    y += 1
+    if y > y_length:
+        break
+    if cave[y][x] == ".":
+        sand_path.append({"x": x, "y": y})
+        continue
+
+    x -= 1
+    if x < 0:
+        break
+    if cave[y][x] == ".":
+        sand_path.append({"x": x, "y": y})
+        continue
+
+    x += 2
+    if x >= x_length:
+        break
+    if cave[y][x] == ".":
+        sand_path.append({"x": x, "y": y})
+        continue
+
+    x -= 1
+    y -= 1
+    sand_count += 1
+    cave[y][x] = "O"
+    sand_path.pop()
+
+sand_path.pop(0)
+for sand in sand_path:
+    cave[sand["y"]][sand["x"]] = "~"
+
+for i in range(len(cave)):
+    line = cave[i]
+    print("".join(line) + " " + f'{i:03}')
+
+print()
+print(sand_count)
