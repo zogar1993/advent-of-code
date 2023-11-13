9185ced257785d2efdf842b140289998cc04d23e
Facundo Garcia <facundogarcia1993@gmail.com>
2023-11-13 20:41:07 -0300

    complete 2015_1_2

---
+++ 2015/src/1/1.zig
@@ -0,0 +1,30 @@
+const fs = @import("std").fs;
+const heap = @import("std").heap;
+const io = @import("std").io;
+const fmt = @import("std").fmt;
+
+pub fn run() !void {
+    const path = "src/1/data.txt";
+    var file = try fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const read_buf = try file.readToEndAlloc(allocator, 1024 * 1024);
+    defer allocator.free(read_buf);
+
+    var floor: i32 = 0;
+    for (read_buf) |character| {
+        if (character == '(') {
+            floor += 1;
+        } else {
+            floor -= 1;
+        }
+    }
+
+    const x = try fmt.allocPrint(allocator, "{d}\n", .{floor});
+    const stdout = io.getStdOut();
+    _ = try stdout.writeAll(x);
+}
--- 2015/src/main.zig
+++ 2015/src/main.zig
@@ -2,31 +2,12 @@ const fs = @import("std").fs;
 const heap = @import("std").heap;
 const io = @import("std").io;
 const std = @import("std");
+const f11 = @import("1/1.zig");
+const f12 = @import("1/2.zig");
 
 pub fn main() !void {
-    const path = "src/1/data.txt";
-    var file = try fs.cwd().openFile(path, .{});
-    defer file.close();
-
-    var arena = heap.ArenaAllocator.init(heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    const read_buf = try file.readToEndAlloc(allocator, 1024 * 1024);
-    defer allocator.free(read_buf);
-
-    var floor: i32 = 0;
-    for (read_buf) |character| {
-        if (character == '(') {
-            floor += 1;
-        } else {
-            floor -= 1;
-        }
-    }
-
-    const x = try std.fmt.allocPrint(allocator, "{d}", .{floor});
-    const stdout = io.getStdOut();
-    _ = try stdout.writeAll(x);
+    try f11.run();
+    try f12.run();
 
     //// Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     //std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
